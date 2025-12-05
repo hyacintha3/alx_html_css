c77565b4326b0ed03fa37331cf3224a6fb2635d0
Hyacintha NIYONIZEYE <niyohyacintha@gmail.com>
2025-12-05 20:18:16 +0200

    html and css tags

--- headphones/8-script.js
+++ headphones/8-script.js
@@ -1,8 +1,6 @@
-const hamburger = document.getElementById('hamburger');
-const nav = document.getElementById('mainNav');
+const hamburger = document.getElementById("hamburger");
+const nav = document.getElementById("nav");
 
-hamburger.addEventListener('click', () => {
-  const expanded = hamburger.getAttribute('aria-expanded') === 'true';
-  hamburger.setAttribute('aria-expanded', String(!expanded));
-  nav.classList.toggle('open');
+hamburger.addEventListener("click", () => {
+  nav.classList.toggle("show");
 });
